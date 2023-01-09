f7d6ab72eac9cb6806463cd8e7570dce916e0980
shani barak <shanibrk@gmail.com>
2023-01-09 10:08:44 +0200

    simple structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Board from "./components/Board";
+import Keyboard from "./components/keyboard/Keyboard";
 
 function App() {
-  return <div className="App">dfdf</div>;
+  return (
+    <div className="App">
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <Board />
+      <Keyboard />
+    </div>
+  );
 }
 
 export default App;
